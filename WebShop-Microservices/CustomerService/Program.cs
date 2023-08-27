311e5f6745d6e24966074551e57ebca4fbebe258
Teodora Gavrilovic <57232397+luka-miljkovic@users.noreply.github.com>
2023-08-27 23:06:55 +0200

    Customer Service created
    Customer Service - Microservice which is respond for managing with customer data

---
+++ WebShop-Microservices/CustomerService/Program.cs
@@ -0,0 +1,29 @@
+using CustomerService;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+/* Database Context Dependency Injection */
+//var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+//var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+//var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+var dbHost = "localhost";
+var dbName = "dms_customers";
+var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+builder.Services.AddDbContext<CustomerDbContext>(opt => opt.UseSqlServer(connectionString));
+/* ===================================== */
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
