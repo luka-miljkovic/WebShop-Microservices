c582dcc9154af7e3d91d49ff3aa9484783f0e9e1
Teodora Gavrilovic <57232397+luka-miljkovic@users.noreply.github.com>
2023-08-28 00:10:46 +0200

    Customer Service added to Docker Compose
    Customer Service API and Database containerization
    Database used for this service is SQL Server

--- WebShop-Microservices/CustomerService/Program.cs
+++ WebShop-Microservices/CustomerService/Program.cs
@@ -8,12 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 /* Database Context Dependency Injection */
-//var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
-//var dbName = Environment.GetEnvironmentVariable("DB_NAME");
-//var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
-var dbHost = "localhost";
-var dbName = "dms_customers";
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
 var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+//var dbHost = "localhost";
+//var dbName = "dms_customers";
+//var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
 var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 builder.Services.AddDbContext<CustomerDbContext>(opt => opt.UseSqlServer(connectionString));
 /* ===================================== */
